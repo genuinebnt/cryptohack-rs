1f4815f767b1e981840fd0b5df4e4fcf72364a78
genuinebasilnt <genuinebnt@gmail.com>
2025-03-09 19:29:00 +0530

    intro:bytes_and_big_integers

--- src/intro.rs
+++ src/intro.rs
@@ -1,22 +1,45 @@
 #[cfg(test)]
 mod tests {
-    use crate::utils::{ascii_chars_to_string, decode_hex};
+    use base64::prelude::*;
+    use num_bigint::BigUint;
+    use num_traits::Num;
+
+    use crate::utils::{bytes_to_string, hex_to_bytes, long_to_bytes};
 
     #[test]
-    fn solve_intro_ascii() {
+    fn solve_ascii() {
         let input = [
             99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52,
             98, 108, 51, 125,
         ];
-        let got = ascii_chars_to_string(&input);
+        let got = bytes_to_string(&input);
         let want = "crypto{ASCII_pr1nt4bl3}";
         assert_eq!(got, want);
     }
 
     #[test]
-    fn solve_intro_hex() {
+    fn solve_hex() {
         let input ="63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d";
-        let output = ascii_chars_to_string(&decode_hex(input).unwrap());
+        let output = bytes_to_string(&hex_to_bytes(input).unwrap());
         assert_eq!(output, "crypto{You_will_be_working_with_hex_strings_a_lot}");
+    }
+
+    #[test]
+    fn solve_base64() {
+        let input = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf";
+        let output = hex_to_bytes(input).unwrap();
+        assert_eq!(
+            BASE64_STANDARD.encode(output),
+            "crypto/Base+64+Encoding+is+Web+Safe/"
+        );
+    }
+
+    #[test]
+    fn solve_bytes_and_big_integers() {
+        let long =
+            "11515195063862318899931685488813747395775516287289682636499965282714637259206269";
+        let long = BigUint::from_str_radix(long, 10).unwrap();
+        let bytes = long_to_bytes(long).unwrap();
+        assert_eq!(bytes_to_string(&bytes), "crypto{3nc0d1n6_4ll_7h3_w4y_d0wn}");
     }
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,12 +1,39 @@
 use std::num::ParseIntError;
 
-pub fn ascii_chars_to_string(input: &[u8]) -> String {
+use hex::FromHexError;
+use num_bigint::BigUint;
+
+pub fn bytes_to_string(input: &[u8]) -> String {
     input.iter().map(|v| *v as char).collect()
 }
 
-pub fn decode_hex(input: &str) -> Result<Vec<u8>, ParseIntError> {
+pub fn hex_to_bytes(input: &str) -> Result<Vec<u8>, ParseIntError> {
     (0..input.len())
         .step_by(2)
         .map(|i| u8::from_str_radix(&input[i..i + 2], 16))
         .collect()
+}
+
+pub fn bytes_to_long(input: &[u8]) -> Result<u64, ParseIntError> {
+    let hex_str = hex::encode(input);
+    u64::from_str_radix(&hex_str, 16)
+}
+
+pub fn long_to_bytes(input: BigUint) -> Result<Vec<u8>, ParseIntError> {
+    let hex_str = input.to_str_radix(16);
+    (0..hex_str.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&hex_str[i..i + 2], 16))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bytes_to_long() {
+        let bytes = [72, 69, 76, 76, 79];
+        assert_eq!(bytes_to_long(&bytes), Ok(310400273487))
+    }
 }
